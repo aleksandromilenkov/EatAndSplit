e6d0cd5268f1fc9562d41fa927c541f85a956c22
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2024-04-01 23:50:03 +0200

    fixing text color

--- src/Components/Friend.js
+++ src/Components/Friend.js
@@ -9,11 +9,11 @@ const Friend = ({ friend }) => {
       <p>You and {friend.name} are even</p>
     ) : friend.balance < 0 ? (
       <p className="red">
-        You owe {friend.name} {Math.abs(friend.balance)}€
+        You owe {friend.name} {Math.abs(friend.balance)}$
       </p>
     ) : (
       <p className="green">
-        {friend.name} owes you {Math.abs(friend.balance)}€
+        {friend.name} owes you {Math.abs(friend.balance)}$
       </p>
     );
   return (
